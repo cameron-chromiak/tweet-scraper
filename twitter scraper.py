3e3d4ad78f8d108306bcdf3a6da8bd9a20f462fa
cameron-chromiak <cam.chromiak@hotmail.com>
2019-01-27 16:41:19 -0500

    wrapped in a function. added capability to search multiple users at once

--- twitter scraper.py
+++ twitter scraper.py
@@ -12,20 +12,27 @@ consumer_secret = 'ReI1JoVs0swujvMW3RyoMmVZoc35h7ZuYU5SPTPvnrCA4sdUuT'
 access_token = '1087755752763285504-AIpyDSITUSnGTIBwxZF8a8G5Pxj6EE'
 access_token_secret = 'tHpLltK4btp196PT7Wr5rLAhk3IWhHi1yev2OTuGTkUhX'
 
-# OAuth process, using the keys and tokens
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+def getTweets(target_users):
+    # OAuth process, using the keys and tokens
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
 
-# Creation of the actual interface, using authentication
-api = tweepy.API(auth)
-target_user = 'realDonaldTrump'
+    # Creation of the actual interface, using authentication
+    api = tweepy.API(auth)
 
-tweets = api.user_timeline(target_user)
-# Empty Array
-tmp=[]
 
-tweets_text = [tweet.text for tweet in tweets]
-for j in tweets_text:
-    tmp.append(j)
 
-print(tmp)
+    word_data = []
+    for user in target_users:
+        tweets = api.user_timeline(user)
+        tweets_text = [tweet.text.encode("utf-8") for tweet in tweets]
+        for j in tweets_text:
+            word_data.append(j)
+
+    print(word_data)
+
+def main():
+    target_users = ['realDonaldTrump', 'HillaryClinton']
+    getTweets(target_users)
+
+main()
